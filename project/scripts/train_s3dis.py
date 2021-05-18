6c632bd7e8687e498a8ad4ac8794f3149c61e4a5
PointCloudYC <cyinac@connect.ust.hk>
2021-05-18 22:27:08 +0800

    DL pipeline step 1: config

---
+++ project/scripts/train_s3dis.py
@@ -0,0 +1,151 @@
+"""
+Distributed training script for semantic segmentation on S3DIS dataset
+"""
+import os
+import sys
+import time
+from datetime import datetime
+import json
+import random
+import numpy as np
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(ROOT_DIR)
+
+# configs and logging
+import argparse
+from utils.config import config, update_config
+from utils.logger import setup_logger
+
+# pytorch
+import torch
+import torch.nn as nn
+from torchvision import transforms
+import torch.distributed as dist
+from torch.utils.tensorboard import SummaryWriter
+from torch.nn.parallel import DistributedDataParallel
+
+# import datasets.data_utils as d_utils
+# from models import build_scene_segmentation # models/build.py
+# from datasets import S3DISSeg
+
+# metrics and lr scheduler
+from utils.util import AverageMeter, s3dis_metrics, sub_s3dis_metrics, s3dis_part_metrics
+from utils.lr_scheduler import get_scheduler
+
+
+def parse_config():
+    """load configs including parameters from dataset, model, training, etc.
+    The basic process is:
+    - load default settings based on the config dict in the utils/config.py
+    - update the config dict using yaml file specified by an argparse argument(--cfg argument)
+    - update the config dict using argparse arguments
+
+    Returns:
+        tuple: (args, config) contains config settings where args is argparse.Namespace object while config is a dict
+    """
+    parser = argparse.ArgumentParser('S3DIS semantic segmentation training')
+    parser.add_argument('--cfg', type=str, default='project/cfgs/s3dis/pointnet.yaml', help='config file')
+    parser.add_argument('--data_root', type=str, default='data', help='root director of dataset')
+    parser.add_argument('--num_workers', type=int, default=4, help='num of workers to use')
+    parser.add_argument('--batch_size', type=int, help='batch_size')
+    parser.add_argument('--num_points', type=int, help='num_points')
+    parser.add_argument('--num_steps', type=int, help='num_steps')
+    parser.add_argument('--base_learning_rate', type=float, help='base learning rate')
+    parser.add_argument('--weight_decay', type=float, help='weight_decay')
+    parser.add_argument('--epochs', type=int, help='number of training epochs')
+    parser.add_argument('--start_epoch', type=int, help='used for resume')
+
+    # io
+    parser.add_argument('--load_path', default='', type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: none)')
+    parser.add_argument('--print_freq', type=int, default=10, help='print frequency')
+    parser.add_argument('--save_freq', type=int, default=10, help='save frequency')
+    parser.add_argument('--val_freq', type=int, default=10, help='val frequency')
+    parser.add_argument('--log_dir', type=str, default='log', help='log dir [default: log]')
+
+    # misc
+    parser.add_argument("--local_rank", type=int,default=0, help='local rank for DistributedDataParallel')
+    parser.add_argument("--rng_seed", type=int, default=0, help='manual seed')
+
+    args, unparsed = parser.parse_known_args()
+
+    # update config dict with the yaml file
+    update_config(args.cfg)
+
+    # update config dict with args arguments
+    config.data_root = args.data_root
+    config.num_workers = args.num_workers
+    config.load_path = args.load_path
+    config.print_freq = args.print_freq
+    config.save_freq = args.save_freq
+    config.val_freq = args.val_freq
+    config.rng_seed = args.rng_seed
+
+    config.local_rank = args.local_rank
+    
+    model_name = args.cfg.split('.')[-2].split('/')[-1] # model name, e.g., pointnet
+    current_time = datetime.now().strftime('%Y%m%d%H%M%S') #20210518221044 means 2021, 5.18, 22:10:44
+    config.log_dir = os.path.join(args.log_dir, 's3dis', f'{model_name}_{int(current_time)}') ## log_dir=log/s3dis/pointnet_time 
+
+    if args.batch_size:
+        config.batch_size = args.batch_size
+    if args.num_points:
+        config.num_points = args.num_points
+    if args.num_steps:
+        config.num_steps = args.num_steps
+    if args.base_learning_rate:
+        config.base_learning_rate = args.base_learning_rate
+    if args.weight_decay:
+        config.weight_decay = args.weight_decay
+    if args.epochs:
+        config.epochs = args.epochs
+    if args.start_epoch:
+        config.start_epoch = args.start_epoch
+
+    print(args)
+    print(config)
+
+    torch.manual_seed(args.rng_seed)
+    torch.cuda.manual_seed_all(args.rng_seed)
+    random.seed(args.rng_seed)
+    np.random.seed(args.rng_seed)
+
+    return args, config
+
+
+def main(config):
+    pass
+    # model
+
+    # loss and optimizer
+
+    # training loop
+    # evaluation loop
+
+
+if __name__ == "__main__":
+
+    # load config
+    args, config = parse_config()
+
+    torch.cuda.set_device(config.local_rank)
+    torch.distributed.init_process_group(backend='nccl', init_method='env://')
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = True
+
+    os.makedirs(args.log_dir, exist_ok=True)
+    os.environ["JOB_LOG_DIR"] = config.log_dir
+
+    logger = setup_logger(output=config.log_dir, distributed_rank=dist.get_rank(), name="s3dis")
+    if dist.get_rank() == 0:
+        path = os.path.join(config.log_dir, "config.json")
+        # save args and config settings to config.json
+        with open(path, 'w') as f:
+            json.dump(vars(args), f, indent=2)
+            json.dump(vars(config), f, indent=2)
+            os.system('cp %s %s' % (args.cfg, config.log_dir))
+        logger.info("Full config saved to {}".format(path))
+    main(config)
--- project/utils/config.py
+++ project/utils/config.py
@@ -2,6 +2,7 @@ import yaml
 from easydict import EasyDict as edict
 
 config = edict()
+
 # ---------------------------------------------------------------------------- #
 # Training options
 # ---------------------------------------------------------------------------- #
@@ -17,19 +18,17 @@ config.lr_decay_rate = 0.7
 config.weight_decay = 0
 config.momentum = 0.9
 config.grid_clip_norm = -1
+
 # ---------------------------------------------------------------------------- #
 # Model
 # ---------------------------------------------------------------------------- #
-config.backbone = 'resnet'
-config.head = 'resnet_cls'
+config.backbone = 'mlp'
+config.head = 'mlp_cls'
 config.radius = 0.05
 config.sampleDl = 0.02
 config.density_parameter = 5.0
 config.nsamples = []
 config.npoints = []
-config.width = 144
-config.depth = 2
-config.bottleneck_ratio = 2
 config.bn_momentum = 0.1
 
 # ---------------------------------------------------------------------------- #
@@ -44,6 +43,7 @@ config.batch_size = 32
 config.num_points = 5000
 config.num_classes = 40
 config.num_workers = 4
+
 # data augmentation
 config.x_angle_range = 0.0
 config.y_angle_range = 0.0
@@ -74,33 +74,33 @@ config.rng_seed = 0
 # ---------------------------------------------------------------------------- #
 # Local Aggregation options
 # ---------------------------------------------------------------------------- #
-config.local_aggregation_type = 'pospool'  # pospool, continuous_conv
-# PosPool
-config.pospool = edict()
-config.pospool.position_embedding = 'xyz'
-config.pospool.reduction = 'sum'
-config.pospool.output_conv = False
-# adaptive_weight
-config.adaptive_weight = edict()
-config.adaptive_weight.weight_type = 'dp'  # dp, df, dp_df, fj, dp_fj, fi_df, dp_fi_df, rscnn
-config.adaptive_weight.num_mlps = 1
-config.adaptive_weight.shared_channels = 1
-config.adaptive_weight.weight_softmax = False
-config.adaptive_weight.reduction = 'avg'  # sum_conv, max_conv, mean_conv
-config.adaptive_weight.output_conv = False
-# pointwisemlp
-config.pointwisemlp = edict()
-config.pointwisemlp.feature_type = 'dp_fj'  # dp_fj, fi_df, dp_fi_df
-config.pointwisemlp.num_mlps = 1
-config.pointwisemlp.reduction = 'max'
-# pseudo_grid
-config.pseudo_grid = edict()
-config.pseudo_grid.fixed_kernel_points = 'center'
-config.pseudo_grid.KP_influence = 'linear'
-config.pseudo_grid.KP_extent = 1.0
-config.pseudo_grid.num_kernel_points = 15
-config.pseudo_grid.convolution_mode = 'sum'
-config.pseudo_grid.output_conv = False
+# config.local_aggregation_type = 'pospool'  # pospool, continuous_conv
+# # PosPool
+# config.pospool = edict()
+# config.pospool.position_embedding = 'xyz'
+# config.pospool.reduction = 'sum'
+# config.pospool.output_conv = False
+# # adaptive_weight
+# config.adaptive_weight = edict()
+# config.adaptive_weight.weight_type = 'dp'  # dp, df, dp_df, fj, dp_fj, fi_df, dp_fi_df, rscnn
+# config.adaptive_weight.num_mlps = 1
+# config.adaptive_weight.shared_channels = 1
+# config.adaptive_weight.weight_softmax = False
+# config.adaptive_weight.reduction = 'avg'  # sum_conv, max_conv, mean_conv
+# config.adaptive_weight.output_conv = False
+# # pointwisemlp
+# config.pointwisemlp = edict()
+# config.pointwisemlp.feature_type = 'dp_fj'  # dp_fj, fi_df, dp_fi_df
+# config.pointwisemlp.num_mlps = 1
+# config.pointwisemlp.reduction = 'max'
+# # pseudo_grid
+# config.pseudo_grid = edict()
+# config.pseudo_grid.fixed_kernel_points = 'center'
+# config.pseudo_grid.KP_influence = 'linear'
+# config.pseudo_grid.KP_extent = 1.0
+# config.pseudo_grid.num_kernel_points = 15
+# config.pseudo_grid.convolution_mode = 'sum'
+# config.pseudo_grid.output_conv = False
 
 
 def update_config(config_file):
