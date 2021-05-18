b0f02cca602939f9c866bcdf02d776f4afac08f9
PointCloudYC <cyinac@connect.ust.hk>
2021-05-18 11:59:45 +0800

    init project structure

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project',
-    version='0.0.0',
-    description='Describe Your Cool Project',
-    author='',
-    author_email='',
+    name='PointNet-modern.pytorch',
+    version='0.0.1',
+    description='PointNet impplemented in pytorch with better readability',
+    author='PointCloudYC',
+    author_email='chao.yin@connect.ust.hk',
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url='https://github.com/PyTorchLightning/pytorch-lightning-conference-seed',
-    install_requires=['pytorch-lightning'],
+    url='https://github.com/PointCloudYC/PointNet-modern.pytorch',
+    install_requires=['torch'],
     packages=find_packages(),
 )
 
